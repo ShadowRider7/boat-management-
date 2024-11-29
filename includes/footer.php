fae15da939574232a95aa3179c2088a377ed7b7f
ShadowRider7 <sadhinv@gmail.com>
2024-11-29 10:23:13 +0600

    lol

---
+++ includes/footer.php
@@ -0,0 +1,15 @@
+
+    <div class="footer bg-light">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="copyright">
+                <p>
+                   
+                  Boat Booking System <i class="icon-boat text-danger" aria-hidden="true"></i>
+                    </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
